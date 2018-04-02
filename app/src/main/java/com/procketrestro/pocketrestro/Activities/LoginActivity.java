b827e5b300cfa7bc386319420f2ecf591b2dbabb
mayurpadhye <mayur.padhye033@gmail.com>
2018-04-02 09:15:26 +0530

    iniital commit

---
+++ app/src/main/java/com/procketrestro/pocketrestro/Activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.procketrestro.pocketrestro.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.procketrestro.pocketrestro.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
